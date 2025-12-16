a30b3910b28ea0c8fb1f40ecffe9dafe924f4455
Ronnie Low <ronnielow@protonmail.com>
2025-12-16 18:40:24 +1100

    figured out what happens for program headers w/ static libs

--- tests/static_lib/sum_lib.c
+++ tests/static_lib/sum_lib.c
@@ -3,6 +3,9 @@
 // elf executable binary. What happens if i instead load the program header on a static
 // lib? this is the static lib in squestion
 
+// ANSWER: 
+//  program header entry size:        	0 (bytes)
+//  program header entry num:         	0
 int sum(int a, int b) {
     return a + b;
 }
