70dc27174c9d1b5cc4b039b70994926bd74c4163
Ronnie Low <ronnielow@protonmail.com>
2025-12-16 18:22:00 +1100

    feat: add testing libraries

---
+++ tests/static_lib/sum_lib.c
@@ -0,0 +1,8 @@
+// this is a test library for me to figure out how the elf format works
+// in this case, the "program header" for an elf file is how the OS knows how to load an
+// elf executable binary. What happens if i instead load the program header on a static
+// lib? this is the static lib in squestion
+
+int sum(int a, int b) {
+    return a + b;
+}
